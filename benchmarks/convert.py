55a8860b045a37e77b5b092794f7a1f8e14f113f
Andreas Zeidler <witsch@plone.org>
2009-10-29 14:06:53 +0000

    add results of running the benchmark tests with different numbers of content items (5, 50, 500, 5000) and a helper script for converting them into something mainly intended to be imported into a spreadsheet app, but also slightly more human-readable (refs #9316)
    to run the benchmarks yourself, you might use it like so::
    {{{
    $ bin/instance test -s plone.app.folder --tests-pattern=benchmarks 2>&1 | benchmarks/convert.py
    }}}
    
    svn path=/plone.app.folder/trunk/; revision=30898

---
+++ benchmarks/convert.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from fileinput import input
+from re import compile
+from sys import argv
+
+test = compile(r'^\s*test(.*)(Regular|Large|Ordered) \(')
+time = compile(r'([\d.]*) seconds$')
+
+bench = {}
+keys = set()
+for line in input(argv[1:]):
+    match = test.search(line)
+    if match:
+        name, folder = match.groups()
+        keys.add(folder)
+    match = time.search(line)
+    if match:
+        seconds, = map(float, match.groups())
+        results = bench.setdefault(name, {})
+        results[folder] = seconds
+
+out = [''] + sorted(keys)
+print '\t'.join(map(str, out))
+
+for folder in sorted(bench):
+    out = [folder]
+    for key in sorted(keys):
+        out.append(bench[folder][key])
+    print '\t'.join(map(str, out))
