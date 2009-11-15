84b3127c16e5e4e328406d6813d3fd4e3c36b011
Andreas Zeidler <witsch@plone.org>
2009-11-15 03:01:22 +0000

    tag [http://pypi.python.org/pypi/plone.app.folder/1.0b1 1st] beta
    svn path=/plone.app.folder/trunk/; revision=31570

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from os.path import join
 
-version = '1.0b1'
+version = '1.0b2'
 readme = open(join('README.txt')).read()
 history = open(join('docs', 'HISTORY.txt')).read()
 
