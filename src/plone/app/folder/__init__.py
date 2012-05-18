6fd696719937ae239846b70bd06560f69b53fdf8
David Glick <dglick@gmail.com>
2012-05-18 08:40:46 -0400

    only install the AT content type if Archetypes is present

--- src/plone/app/folder/__init__.py
+++ src/plone/app/folder/__init__.py
@@ -10,24 +10,28 @@ setDefaultRoles(AddFolder, ('Manager', 'Owner'))
 def initialize(context):
     """ initializer called when used as a zope2 product """
 
-    from Products.Archetypes import atapi
-    from Products.CMFCore import utils
+    try:
+        from Products.Archetypes import atapi
+        from plone.app.folder import folder
+    except ImportError:
+        pass
+    else:
+        from Products.CMFCore import utils
 
-    from plone.app.folder import folder
-    folder.__name__    # make pyflakes happy...
+        folder.__name__    # make pyflakes happy...
 
-    content_types, constructors, ftis = atapi.process_types(
-        atapi.listTypes(packageName), packageName)
+        content_types, constructors, ftis = atapi.process_types(
+            atapi.listTypes(packageName), packageName)
 
-    assert len(content_types) == 1, 'only one new folder, please!'
+        assert len(content_types) == 1, 'only one new folder, please!'
 
-    for atype, constructor, fti in zip(content_types, constructors, ftis):
-        utils.ContentInit('%s: %s' % (packageName, atype.portal_type),
-            content_types = (atype,),
-            permission = AddFolder,
-            extra_constructors = (constructor,),
-            fti = (fti,),
-            ).initialize(context)
+        for atype, constructor, fti in zip(content_types, constructors, ftis):
+            utils.ContentInit('%s: %s' % (packageName, atype.portal_type),
+                content_types = (atype,),
+                permission = AddFolder,
+                extra_constructors = (constructor,),
+                fti = (fti,),
+                ).initialize(context)
 
     from plone.app.folder import nogopip
 
