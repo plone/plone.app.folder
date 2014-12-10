b5045ec61edd3685e45423e64e9149eb63bd038f
Tom Gross <itconsense@gmail.com>
2014-12-10 11:04:10 +0100

    ported tests to plone.app.testing

--- src/plone/app/folder/tests/layer.py
+++ src/plone/app/folder/tests/layer.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from Products.CMFCore.utils import getToolByName
-from Products.PloneTestCase.layer import PloneSite
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
+from plone.app.testing.bbb import PTC_FUNCTIONAL_TESTING
 from Testing.ZopeTestCase import app
 from Testing.ZopeTestCase import close
 from Testing.ZopeTestCase import installPackage
@@ -19,42 +21,28 @@ except ImportError:
     from Products.Five import fiveconfigure as metaconfigure
 
 
-class IntegrationLayer(PloneSite):
+class IntegrationLayer(PloneSandboxLayer):
     """ layer for integration tests using the folder replacement type """
 
-    @classmethod
-    def setUp(cls):
-        root = app()
-        portal = root.plone
-        # load zcml & install the package
-        metaconfigure.debug_mode = True
+    defaultBases = (PTC_FUNCTIONAL_TESTING,)
+
+    def setUpZope(self, app, configurationContext):
         from plone.app.folder import tests
-        load_config('testing.zcml', tests)
-        metaconfigure.debug_mode = False
-        installPackage('plone.app.folder', quiet=True)
-        # import replacement profile
-        profile = 'profile-plone.app.folder:default'
-        tool = getToolByName(portal, 'portal_setup')
-        tool.runAllImportStepsFromProfile(profile, purge_old=False)
-        # make sure it's loaded...
+        self.loadZCML('testing.zcml', tests)
+        z2.installProduct(app, 'plone.app.blob')
+
+
+    def setUpPloneSite(self, portal):
+        # restore default workflow
+        applyProfile(portal, 'profile-plone.app.folder:default')
+
         types = getToolByName(portal, 'portal_types')
         assert types.getTypeInfo('Folder').product == 'plone.app.folder'
-        # and commit the changes
-        commit()
-        close(root)
-
-    @classmethod
-    def tearDown(cls):
-        pass
 
 
 class PartialOrderingIntegrationLayer(IntegrationLayer):
     """ layer for integration tests using the partial ordering adapter """
 
-    @classmethod
-    def setUp(cls):
+    def setUpZope(self, app, configurationContext):
+        IntegrationLayer.setUpZope(self, app, configurationContext)
         provideAdapter(PartialOrdering)
-
-    @classmethod
-    def tearDown(cls):
-        pass
