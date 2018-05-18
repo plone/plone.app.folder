b94b558bff8165eea749f822da5d9875797517cb
Jan Mevissen <mevissen@interaktiv.de>
2018-05-18 14:11:56 +0200

    move gopipindex from plone.app.folder to plone.folder https://github.com/plone/Products.CMFPlone/issues/2422

--- src/plone/app/folder/tests/test_integration.py
+++ src/plone/app/folder/tests/test_integration.py
@@ -17,14 +17,3 @@ class FolderReplacementTests(IntegrationTestCase):
         self.assertEqual(self.folder['foo'].getPortalTypeName(), 'Folder')
         from plone.app.folder.base import BaseBTreeFolder
         self.failUnless(isinstance(self.folder['foo'], BaseBTreeFolder))
-
-    def testGetObjPositionInParentIndex(self):
-        from plone.app.folder.nogopip import GopipIndex
-        catalog = self.portal.portal_catalog
-        self.failUnless('getObjPositionInParent' in catalog.indexes())
-        self.failUnless(
-            isinstance(
-                catalog.Indexes['getObjPositionInParent'],
-                GopipIndex
-            )
-        )
