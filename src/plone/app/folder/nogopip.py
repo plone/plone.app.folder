839fd985774c9d0e35dd07d41652fe25dd50c422
Peter Mathis <peter.mathis@kombinat.at>
2018-10-02 13:27:06 +0200

    add missing deprecation paths

--- src/plone/app/folder/nogopip.py
+++ src/plone/app/folder/nogopip.py
@@ -5,4 +5,6 @@ from zope.deferredimport import deprecated
 deprecated(
     "Please import from plone.folder.nogopip",
     GopipIndex='plone.folder.nogopip:GopipIndex',
+    manage_addGopipForm='plone.folder.nogopip:manage_addGopipForm',
+    manage_addGopipIndex='plone.folder.nogopip:manage_addGopipIndex',
 )
